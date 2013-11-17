0047d350af790665f3da70358f423e51bb8a89b5
Idean Labib <idean1.labib@gmail.com>
2013-11-17 11:09:38 -0800

    Secret Token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Discover::Application.config.secret_token = 'a95d9a2121a631403aa8b4162a0505d48fd68b8429397988fa93c6c2e0d2f4412680a5ac56951b20cda357580f63bbe04256442d84dab6b343af3620451aaf12'
