dd507593a67710fd0431a11a3a1abfdcef97598f
Saba9 <Saba1998@gmail.com>
2013-11-22 10:12:09 -0800

    Wrote test for email validity.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe User do
+  context 'signing up' do
+    before (:all) { @user = FactoryGirl.build(:user) } 
+    it 'blank email' do
+      @user.email = ''
+      expect(@user.valid?).to eq(false)
+    end
+  end
 end
