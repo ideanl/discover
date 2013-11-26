bdbbb8595861a0adf3d402f2000edae1863aee15
Saba9 <saba1998@gmail.com>
2013-11-26 09:13:04 -0800

    Added rails relationships.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+	has_many :posts, dependent: :destroy
+	has_one :profile
   before_save { self.email = email.downcase }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
