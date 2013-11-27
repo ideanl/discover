aa74401a8e8bbca4586eb59bb1631b63ae76a9d4
Saba9 <saba1998@gmail.com>
2013-11-27 08:29:32 -0800

    Added create and new to posts model and added user_id column!

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,19 @@
 class PostsController < ApplicationController
   def new
-
+		@post = Post.new	
   end
+
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			flash[:success] = "Post successfully create!"
+		else
+			flash.now[:danger] = "Post could not be created. Error: #{@user.errors.full_messages.join('. ')}"
+			render 'new'
+	end
+
+	private
+	def post_params
+		params.require(:post).permit(:id, :body, :user_id)
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131127160528) do
+ActiveRecord::Schema.define(version: 20131127161548) do
 
   create_table "posts", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "body"
+    t.integer  "user_id"
   end
 
   create_table "profiles", force: true do |t|
